e83fa99318b6019997752c30e64310e981bb95a8
ejurga <ejacc@protonmail.com>
2024-04-10 18:23:47 -0400

    moves functions to generate GRDI list class to data-raw

--- R/grdi.R
+++ R/grdi.R
@@ -13,75 +13,6 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-get_slots <- function(){
-  data("grdi_schema")
-  # Get lot (column) names
-  slots <- grdi_schema[["slots"]]
-  # Regex to id all the columns that have to do with AMR
-  amr_regexes <-
-    c("_resistance_phenotype", "_measurement*", "_laboratory_typing_*",
-      "_vendor_name", "_testing_standard*", "_breakpoint")
-  # Get index of AMR slots
-  amr_index <- unlist(lapply(FUN = grep, X = amr_regexes, x = names(slots)))
-  # Remove AMR slots
-  slots_no_amr <- slots[-amr_index]
-  # Return slots schema with no AMR slots
-  return(slots_no_amr)
-}
-
-get_slots_by_flag <- function(flag = c('recommended', 'required')) {
-  slots <- get_slots()
-  slot_list <- vector()
-  for (slot in names(slots)){
-    # In this schema, the "Required" or "Recommended" flags are set by their
-    # presence only, so a NULL value means they are FALSE
-    if (!is.null(slots[[slot]][[flag]])){
-      slot_list <- append(slot_list, slot)
-    }
-  }
-  return(slots[slot_list])
-}
-
-get_slot_menu_names <- function(slot_name){
-  slots <- get_slots()
-  slot <- slots[[slot_name]]
-  menus <- unlist(list(slot[["any_of"]], slot[["range"]]))
-  return(unname(menus))
-}
-
-get_slot_values <- function(slot_name) {
-  menus <- get_slot_menu_names(slot_name)
-  # Get the values of the menu
-  values <- vector()
-  for (menu in menus){
-    # If one of these, just add them to the values list.
-    if (menu == "WhitespaceMinimizedString") {
-      values <- append(values, menu)
-    # Otherwise, descend into the schema and return the menu items
-    } else {
-      values <-
-        append(values,
-               names(grdi_schema$enums[[menu]][["permissible_values"]]))
-    }
-  }
-  return(values)
-}
-
-get_categories <- function(){
-  x <- grdi_schema$classes$GRDI$slot_usage
-  fields <- names(x)
-  cats <- vector()
-  for (field in fields){
-    category <- x[[field]]$slot_group
-    cats <- append(cats, category)
-  }
-  categories <- list()
-  for ( a in unique(cats) ){
-    categories[[a]] <- fields[cats==a]
-  }
-  return(categories)
-}
-
 get_slots_with_menus <- function(){
   slots_with_menus <- list()
   for (slot_name in names(get_slots())){
@@ -96,7 +27,6 @@ get_slots_with_menus <- function(){
 }
 
 grep_slot_val <- function(col_name, x, ...) {
-  x
   values <- get_slot_values(slot_name = col_name)
   result <- agrep(x = values, pattern = x, value = TRUE, ...)
   return(result)
