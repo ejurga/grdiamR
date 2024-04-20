495705e4fe95cc4240377c7a4f9666ed68b19f32
ejurga <ejacc@protonmail.com>
2024-04-19 20:24:32 -0400

    adds new functions for processing GRDI terms

--- R/grdi.R
+++ R/grdi.R
@@ -125,14 +125,20 @@ get_info <- function(field){
 
 #' Get a list of all the ontology terms used in the GRDI standard.
 #'
+#' Returns a datafram of fields and the terms in those fields
+#'
 #' @export
 get_all_field_ontology_terms <- function(){
-  fields <- get_fields_with_menus()
-  onts <- vector()
-  for (field in fields){
-    onts <- c(onts, grdi$fields[[field]]$values)
+  fields_w_menus <- get_fields_with_menus()
+  results <- list()
+  for (fn in fields_w_menus){
+    results[[fn]] <-
+      tibble(field = fn,
+             terms = as.character(grdi$fields[[fn]]$values))
   }
-  return(unique(onts))
+  df <- bind_rows(results)
+
+  return(df)
 }
 
 #' Search for an ontology term across all the possible GRDI fields.
@@ -155,4 +161,45 @@ agrep_across_all_field_terms <- function(x, ...) {
       print(paste("Field:", field, "-->", results))
     }
   }
+}
+
+#' Extract Ontology ID from GRDI term
+#'
+#' @param x Vector of GRDI terms in the format "Term name \[ONTOLOGY:0000000\]"
+#'
+#' @export
+extract_ont_id <- function(x){
+  sub(x = x, "^.+\\[([A-Za-z_]+)[:_]([A-Z0-9]+)\\]", "\\1:\\2")
+}
+
+#' Get ontology name from an ontology ID
+#'
+#' Given a vector of ontology IDs (In the form of ONTOLOGY:00000000),
+#' returns the lowercase name of the ontology ("ontology")
+#'
+#' @param x vector of ontology IDs
+#'
+#' @export
+extract_ontology_name <- function(x){
+  tolower(sub(x = x, "([A-Za-z_]+):[A-Z0-9]+", "\\1"))
+}
+
+#' Separate GRDI ontology terms into two columsn with the Term and ID
+#'
+#' A tidyverse eval function. Uses [tidyr::separate_wider_regex] to
+#' separate a column of form "Term Name \[ONTOLOGY:00000000\]" in the
+#' Term name in one column and the ID into another
+#'
+#' @param data  a data frame
+#' @param <tidy-select> Column to separate
+#'
+#' @export
+separate_ontology_terms <- function(data, col){
+  separate_wider_regex(data, {{ col }},
+                       patterns = c(Term = "^.+",
+                                    "\\[",
+                                    ID = "[A-Za-z_]+[:_][A-Z0-9]+",
+                                    "\\]$"), too_few = "align_start") %>%
+  mutate(Term = trimws(Term),
+         ID = sub(x = ID, "_", ":"))
 }
