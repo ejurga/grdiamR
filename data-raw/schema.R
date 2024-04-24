e9f1b2e04295db0ab60c91c21c0414bd88aa77db
ejurga <ejacc@protonmail.com>
2024-04-24 18:45:20 -0400

    adds a group flag to internal grdi

--- data-raw/schema.R
+++ data-raw/schema.R
@@ -75,8 +75,11 @@ create_grdi <- function(grdi_schema){
   slots <- get_slots(grdi_schema)
 
   fields <- list()
+  groups <- c()
   for (slot in slots){
     fields[[slot$name]] <- make_field(slot)
+    groups <- c(groups,
+                grdi_schema$classes$GRDI$slot_usage[[slot$name]]$slot_group)
   }
 
   grdi <-
@@ -84,6 +87,7 @@ create_grdi <- function(grdi_schema){
       info = list(
         version = grdi_schema$version
       ),
+      groups = unique(groups),
       fields = fields
   )
 
@@ -107,5 +111,6 @@ download.file(url = grdi_yaml_url,
 	      destfile = "data-raw/schema.yaml",
 	      method = "wget")
 grdi_schema <- yaml::yaml.load_file(input = "data-raw/schema.yaml")
+usethis::use_data(grdi_schema, overwrite = TRUE, internal = TRUE)
 grdi <- create_grdi(grdi_schema)
 usethis::use_data(grdi, overwrite = TRUE)
