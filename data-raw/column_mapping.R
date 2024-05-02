bd4c84f2b6701a737e52e139de2ee3caa617abdd
ejurga <ejacc@protonmail.com>
2024-05-02 15:07:56 -0400

    changes data structure to list, fixes some errors in mapping, adds a qc check

--- data-raw/column_mapping.R
+++ data-raw/column_mapping.R
@@ -1,264 +1,288 @@
+require(tidyverse)
+
 column_mapping <-
-    bind_rows(
-      tibble(
-        col     = "sample_collected_by",
+  list(
+    sample_collected_by =
+      list(
         db_tab  = "collection_information",
         db_col  = "sample_collected_by"
       ),
-      tibble(
-        col     = "purpose_of_sampling",
+    purpose_of_sampling =
+      list(
         db_tab  = "sample_purpose",
         db_col  = "purpose_of_sampling"
       ),
-      tibble(
-        col     = "presampling_activity",
+    presampling_activity =
+      list(
         db_tab  = "sample_activity",
         db_col  = "presampling_activity"
       ),
-      tibble(
-        col     = "specimen_processing",
+    specimen_processing =
+      list(
         db_tab  = "collection_information",
         db_col  = "specimen_processing"
       ),
-      tibble(
-        col     = "geo_loc_name (country)",
+    `geo_loc_name (country)` =
+      list(
         db_tab  = "geo_loc",
         db_col  = "geo_loc_name_country"
       ),
-      tibble(
-        col     = "geo_loc_name (state/province/region)",
+    `geo_loc_name (state/province/region)` =
+      list(
         db_tab  = "geo_loc",
         db_col  = "geo_loc_name_state_province_region"
       ),
-      tibble(
-        col     = "food_product_origin_geo_loc_name (country)",
-        db_tab  = "food_product_origin_country",
-        db_col  = "food_data"
+    `food_product_origin_geo_loc_name (country)` =
+      list(
+        db_tab  = "food_data",
+        db_col  = "food_product_origin_country"
       ),
-      tibble(
-        col     = "host_origin_geo_loc_name (country)",
+    `host_origin_geo_loc_name (country)` =
+      list(
         db_tab  = "hosts",
         db_col  = "host_origin_geo_loc_name_country"
       ),
-      tibble(
-        col     = "sample_collection_date_precision",
+    sample_collection_date_precision =
+      list(
         db_tab  = "collection_information",
         db_col  = "sample_collection_date_precision"
       ),
-      tibble(
-        col     = "environmental_site",
-        db_tab  = "environmental_data_site",
+    environmental_site =
+      list(
+        db_tab  = "environment_data_site",
         db_col  = "environmental_site"
       ),
-      tibble(
-        col     = "water_depth_units",
+    water_depth_units =
+      list(
         db_tab  = NA,
         db_col  = NA
       ),
-      tibble(
-        col     = "sediment_depth_units",
+    sediment_depth_units =
+      list(
         db_tab  = NA,
         db_col  = NA
       ),
-      tibble(
-        col     = "air_temperature_units",
+    air_temperature_units =
+      list(
         db_tab  = NA,
         db_col  = NA
       ),
-      tibble(
-        col     = "water_temperature_units",
+    water_temperature_units =
+      list(
         db_tab  = NA,
         db_col  = NA
       ),
-      tibble(
-        col     = "weather_type",
+    weather_type =
+      list(
         db_tab  = "environment_data_weather_type",
         db_col  = "weather_type"
       ),
-      tibble(
-        col     = "available_data_types",
+    available_data_types =
+      list(
         db_tab  = "environment_data_available_data_type",
         db_col  = "available_data_type"
       ),
-      tibble(
-        col     = "animal_or_plant_population",
-        db_tab  = "environment_data_available_data_type",
+    animal_or_plant_population =
+      list(
+        db_tab  = "environment_data_animal_plant",
         db_col  = "animal_or_plant_population"
       ),
-      tibble(
-        col     = "environmental_material",
+    environmental_material =
+      list(
         db_tab  = "environment_data_material",
         db_col  = "environmental_material"
       ),
-      tibble(
-        col     = "anatomical_material",
+    anatomical_material =
+      list(
         db_tab  = "anatomical_data_material",
         db_col  = "anatomical_material"
       ),
-      tibble(
-        col     = "body_product",
+    body_product =
+      list(
         db_tab  = "anatomical_data_body",
         db_col  = "body_product"
       ),
-      tibble(
-        col     = "anatomical_part",
+    anatomical_part =
+      list(
         db_tab  = "anatomical_data_part",
         db_col  = "anatomical_part"
       ),
-      tibble(
-        col     = "anatomical_region",
-        db_tab  = "anatomical_data_part",
+    anatomical_region =
+      list(
+        db_tab  = "anatomical_data",
         db_col  = "anatomical_region"
       ),
-      tibble(
-        col     = "food_product",
+    food_product =
+      list(
         db_tab  = "food_data_product",
         db_col  = "food_product"
       ),
-      tibble(
-        col     = "food_product_properties",
+    food_product_properties =
+      list(
         db_tab  = "food_data_product_property",
         db_col  = "food_product_property"
       ),
-      tibble(
-        col     = "animal_source_of_food",
+    animal_source_of_food =
+      list(
         db_tab  = "food_data_source",
-        db_col  = "food_data_souce"
+        db_col  = "animal_source_of_food"
       ),
-      tibble(
-        col     = "food_product_production_stream",
+    food_product_production_stream =
+      list(
         db_tab  = "food_data",
         db_col  = "food_product_production_stream"
       ),
-      tibble(
-        col     = "collection_device",
+    collection_device =
+      list(
         db_tab  = "collection_information",
         db_col  = "collection_device"
       ),
-      tibble(
-        col     = "collection_method",
+    collection_method =
+      list(
         db_tab  = "collection_information",
         db_col  = "collection_method"
       ),
-      tibble(
-        col     = "food_packaging",
+    food_packaging =
+      list(
         db_tab  = "food_data_packaging",
         db_col  = "food_packaging"
       ),
-      tibble(
-        col     = "host (common name)",
+    `host (common name)` =
+      list(
         db_tab  = "hosts",
         db_col  = "host_common_name"
       ),
-      tibble(
-        col     = "host (scientific name)",
+    `host (scientific name)` =
+      list(
         db_tab  = "hosts",
         db_col  = "host_scientific_name"
       ),
-      tibble(
-        col     = "host (food production name)",
+    `host (food production name)` =
+      list(
         db_tab  = "hosts",
         db_col  = "host_food_production_name"
       ),
-      tibble(
-        col     = "host_age_bin",
+    host_age_bin =
+      list(
         db_tab  = "hosts",
         db_col  = "host_age_bin"
       ),
-      tibble(
-        col     = "organism",
+    organism =
+      list(
         db_tab  = "isolates",
         db_col  = "organism"
       ),
-      tibble(
-        col     = "taxonomic_identification_process",
+    taxonomic_identification_process =
+      list(
         db_tab  = "isolates",
         db_col  = "taxonomic_identification_process"
       ),
-      tibble(
-        col     = "sequenced_by",
+    sequenced_by =
+      list(
         db_tab  = "sequencing",
         db_col  = "sequenced_by"
       ),
-      tibble(
-        col     = "purpose_of_sequencing",
+    purpose_of_sequencing =
+      list(
         db_tab  = "sequencing_purpose",
         db_col  = "purpose_of_sequencing"
       ),
-      tibble(
-        col     = "sequencing_platform",
+    sequencing_platform =
+      list(
         db_tab  = "sequencing",
         db_col  = "sequencing_platform"
       ),
-      tibble(
-        col     = "sequencing_instrument",
+    sequencing_instrument =
+      list(
         db_tab  = "sequencing",
         db_col  = "sequencing_instrument"
       ),
-      tibble(
-        col     = "sequencing_assay_type",
+    sequencing_assay_type =
+      list(
         db_tab  = NA,
         db_col  = NA
       ),
-      tibble(
-        col     = "sequence_submitted_by",
+    sequence_submitted_by =
+      list(
         db_tab  = "public_repository_information",
         db_col  = "sequence_submitted_by"
       ),
-      tibble(
-        col     = "attribute_package",
+    attribute_package =
+      list(
         db_tab  = "public_repository_information",
         db_col  = "attribute_package"
       ),
-      tibble(
-        col     = "stage_of_production",
+    stage_of_production =
+      list(
         db_tab  = "risk_assessment",
         db_col  = "stage_of_production"
       ),
-      tibble(
-        col     = "experimental_intervention",
-        db_tab  = "risk_assessment",
+    experimental_intervention =
+      list(
+        db_tab  = "risk_activity",
         db_col  = "experimental_intervention"
       ),
-      tibble(
-        col     = "AMR_testing_by",
+    AMR_testing_by =
+      list(
         db_tab  = "amr_info",
         db_col  = "amr_testing_by"
       ),
-      tibble(
-        col     = "antimicrobial_phenotype",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_phenotype =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "antimicrobial_phenotype"
       ),
-      tibble(
-        col     = "antimicrobial_measurement_units",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_measurement_units =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "measurement_units"
       ),
-      tibble(
-        col     = "antimicrobial_measurement_sign",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_measurement_sign =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "measurement_sign"
       ),
-      tibble(
-        col     = "antimicrobial_laboratory_typing_method",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_laboratory_typing_method =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "laboratory_typing_method"
       ),
-      tibble(
-        col     = "antimicrobial_laboratory_typing_platform",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_laboratory_typing_platform =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "laboratory_typing_platform"
       ),
-      tibble(
-        col     = "antimicrobial_vendor_name",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_vendor_name =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "vendor_name"
       ),
-      tibble(
-        col     = "antimicrobial_testing_standard",
-        db_tab  = "amr_antibiotices_profile",
+    antimicrobial_testing_standard =
+      list(
+        db_tab  = "amr_antibiotics_profile",
         db_col  = "testing_standard"
       )
     )
+
+
+
+# Test it
+vmr <-
+  DBI::dbConnect(drv = RPostgres::Postgres(),
+                 dbname = "vmr_test",
+                 user = "emil")
+
+fk <- DBI::dbReadTable(vmr, "foreign_keys") %>% as_tibble()
+
+err <- c()
+for (col in names(column_mapping)){
+  x <- column_mapping[[col]]
+  y <- fk %>% filter(table_name==x$db_tab, column_name==x$db_col)
+  if (nrow(y)!=1){
+    message("error on ", col)
+    err <- c(err, col)
+  }
+}
+error_list <- column_mapping[match(err, names(column_mapping))]
+
 usethis::use_data(column_mapping, overwrite = TRUE)
